1d6ba178af19da46ae4556cfab34b94a251e4f6f
browar777 <browar777@gmail.com>
2015-08-21 10:45:31 +0200

    dodaje kalkulator kalk.php

---
+++ kalk.php
@@ -0,0 +1,66 @@
+
+
+
+<!DOCTYPE html>
+<html>
+      <head>
+          <meta charset="UTF-8">
+      </head>
+      <body style="background:gray">
+	  
+	  <div style="width:1000px; height:100%; margin:0 auto; border:solid 1px black;">
+       <h1>   Moja pierwsza strona internetowa.</h1>
+	   </h1>
+	   
+	   <form action="" method="POST">
+	   
+	  X: <input type="text"  name="x"/>
+	   Y: <input type="text"  name="y"/>
+	   
+	   <button type="submit" name="value" value="=">-</button>
+	   <button type="submit" name="value" value="+">+</button>
+	   <button type="submit" name="value" value="*">*</button>
+	   <button type="submit" name="value" value="/">/</button>
+	   
+	   </form>
+	   
+	   <h1>Wynik :
+
+<?PHP
+
+	if(!empty($_POST))
+	{
+		
+		switch ($_POST['value']) 
+{
+	case '+':
+        echo $_POST['x'] + $_POST['y'];
+        break;
+    case '-':
+        echo $_POST['x'] - $_POST['y'];
+        break;
+    case '*':
+        echo $_POST['x'] * $_POST['y'];
+        break;
+	case '/':
+        echo $_POST['x'] / $_POST['y'];
+        break;
+}
+		
+		
+	}
+	
+	echo '</br>';
+	
+	$char = '+';
+	
+$c = eval("return $x $char $z;");
+echo $c;
+?>
+
+
+
+	   </h1>
+	   
+      </body>
+</html>
